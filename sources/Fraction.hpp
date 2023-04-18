dbeea310f2b8df04a3a3dfe8e5d7cb970918d40f
root <root@DESKTOP-PUNVNS3.localdomain>
2023-04-18 08:36:01 +0300

    final hpp file

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -7,25 +7,32 @@ namespace ariel{
         Fraction(int numerator, int denominator);
         int getNumerator();
         int getDenominator();
-        //overloading + operator
+        
+        //arithmetic operators
         Fraction operator+(const Fraction& other) const;
-       //overloading - operator
         Fraction operator-(const Fraction& other) const;
-        //overloading * operator
         Fraction operator*(const Fraction& other) const;
-        //overloading / operator
         Fraction operator/(const Fraction& other) const;
-        //overloading == operator
-        Fraction operator==(const Fraction& other) const;
-        //overloading ++ prefix operator
+        
+        //comparison operators
+        bool operator==(const Fraction& other) const;
+        bool operator!=(const Fraction& other) const;
+        bool operator>(const Fraction& other) const;
+        bool operator>=(const Fraction& other) const;
+        bool operator<(const Fraction& other) const;
+        bool operator<=(const Fraction& other) const;
+        
+        //increment and decrement methods
         Fraction& operator++() const;
-        //overloading ++ postfix operator
-        Fraction operator++() const;
-        //overloading -- prefix operator
+        Fraction operator++(int) const;
         Fraction& operator--() const;
-        //overloading -- postfix operator
-        Fraction operator--() const;
+        Fraction operator--(int) const;
 
+        //output/input objects to an/from ostream object
+        //information for the following methods was taken from-
+        //https://learn.microsoft.com/en-us/cpp/standard-library/overloading-the-output-operator-for-your-own-classes?view=msvc-170
+        friend ostream& operator<<(ostream& os, const Fraction& obj);
+        friend ostream& operator>>(ostream& os, const Fraction& f);
         
         private:
         int numerator;
